e4b1b63b29e1f246dc7ce5497e3ef55881491a57
Nagavamshikrishna <nagajn22@gmail.com>
2023-07-04 13:38:11 -0500

    updated code

--- assignments/A08/api.py
+++ assignments/A08/api.py
@@ -1,22 +1,16 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import csv
-
-
 
 description = """🚀
 ## 4883 Software Tools
 ### Where awesomeness happens
 """
 
-
 app = FastAPI(
-
     description=description,
-
 )
 
 db = []
@@ -37,88 +31,196 @@ with open('data.csv', 'r') as file:
 
 def getUniqueCountries():
     global db
-    countries = {}
+    country = {}
 
     for row in db:
-        print(row)
-        if not row[2] in countries:
-            countries[row[2]] = 0
+        if not row[2] in country:
+            country[row[2]] = 0
 
-    return list(countries.keys())
+    return list(country.keys())
 
-def getUniqueWhos():
+
+def getUniqueRegions():
     global db
-    whos = {}
+    regions = {}
 
     for row in db:
-        print(row)
-        if not row[3] in whos:
-            whos[row[3]] = 0
-   
-    return list(whos.keys())
+        if not row[3] in regions:
+            regions[row[3]] = 0
+
+    return list(regions.keys())
+
+
+def calculateDeaths(country=None, region=None, year=None):
+    global db
+    deaths = 0
+
+    for row in db:
+        if country is not None and row[2] != country:
+            continue
+        if region is not None and row[3] != region:
+            continue
+        if year is not None and row[0][:4] != year:
+            continue
+
+        deaths += int(row[6])
+
+    return deaths
+
+
+def calculateDeaths(country=None, region=None, year=None):
+    global db
+    cases = 0
+
+    for row in db:
+        if country is not None and row[2] != country:
+            continue
+        if region is not None and row[3] != region:
+            continue
+        if year is not None and row[0][:4] != year:
+            continue
+
+        cases += int(row[4])
+
+    return cases
+
+
+def findCountryWithMaxDeaths(min_date=None, max_date=None):
+    global db
+    max_deaths = 0
+    country_with_max_deaths = None
+
+    for row in db:
+        if min_date is not None and row[0] < min_date:
+            continue
+        if max_date is not None and row[0] > max_date:
+            continue
+
+        deaths = int(row[5])
+        if deaths > max_deaths:
+            max_deaths = deaths
+            country_with_max_deaths = row[2]
+
+    return country_with_max_deaths
+
+
+def findCountryWithMinDeaths(min_date=None, max_date=None):
+    global db
+    min_deaths = float('inf')
+    country_with_min_deaths = None
+
+    for row in db:
+        if min_date is not None and row[0] < min_date:
+            continue
+        if max_date is not None and row[0] > max_date:
+            continue
+
+        deaths = int(row[5])
+        if deaths < min_deaths:
+            min_deaths = deaths
+            country_with_min_deaths = row[2]
+
+    return country_with_min_deaths
+
+
+def calculateAverageDeaths():
+    global db
+    total_deaths = 0
+    num_countries = len(getUniqueCountries())
+
+    for row in db:
+        total_deaths += int(row[5])
+
+    return total_deaths / num_countries
+
 
 @app.get("/")
 async def docs_redirect():
-    """Api's base route that displays the information created above in the ApiInfo section."""
+    """Retrieves the documentation provided by Swagger."""
     return RedirectResponse(url="/docs")
 
-@app.get("/countries/")
-async def countries():
 
-    return {"countries":getUniqueCountries()}
-
-
-@app.get("/whos/")
-async def whos():
-
-    return {"whos":getUniqueWhos()}
-
-@app.get("/casesByRegion/")
-async def casesByRegion(year:int = None):
-    """
-    Returns the number of cases by region
-    ## Hello world
-    - 1
-    - 2
-    - 3
-    """
-
-    cases = {}
-    
-    for row in db:
-        if year != None and year != int(row[0][:4]):
-            continue
-            
-        if not row[3] in cases:
-            cases[row[3]] = 0
-        cases[row[3]] += int(row[4])    
-
-    return {"data":cases,"success":True,"message":"Cases by Region","size":len(cases),"year":year}
-
-
-
-my_list = ['apple', 'banana', 'cherry', 'date', 'elderberry']
-
-@app.get("/get_values1/")
-def get_values1(index1: int=None, index2: int=None):
+@app.get("/countries")
+async def get_countries():
+    """Retrieves a list of unique countries from the 'db'."""
     try:
-        value1 = my_list[index1]
-        value2 = my_list[index2]
-        return [value1, value2]
-    except IndexError:
-        return {"error": "Invalid index provided."}
+        countries = getUniqueCountries()
+        return {"success": True, "countries": countries}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
 
 
-@app.get("/get_values2/{index1}/{index2}")
-def get_values2(index1: int, index2: int):
+@app.get("/regions")
+async def get_regions():
+    """Retrieves a list of available WHO regions from the 'db'."""
     try:
-        value1 = my_list[index1]
-        value2 = my_list[index2]
-        return [value1, value2]
-    except IndexError:
-        return {"error": "Invalid index provided."}
-    
+        regions = getUniqueRegions()
+        return {"success": True, "regions": regions}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
+
+
+@app.get("/deaths")
+async def get_total_deaths(country: str = Query(None), region: str = Query(None), year: str = Query(None)):
+    """
+    Retrieves the total deaths for the given country, region, and year.
+    If no parameters are provided, returns the total deaths for all countries.
+    """
+    try:
+        deaths = calculateDeaths(country, region, year)
+        return {"success": True, "deaths": deaths}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
+
+
+@app.get("/cases")
+async def get_total_deaths(country: str = Query(None), region: str = Query(None), year: str = Query(None)):
+    """
+    Retrieves the total deaths for the given country, region, and year.
+    If no parameters are provided, returns the total deaths for all countries.
+    """
+    try:
+        cases = calculateDeaths(country, region, year)
+        return {"success": True, "cases": cases}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
+
+
+@app.get("/max_deaths")
+async def get_country_with_max_deaths(min_date: str = Query(None), max_date: str = Query(None)):
+    """
+    Finds the country with the most deaths.
+    If min_date and max_date are provided, finds the country with the most deaths between the specified range of dates.
+    """
+    try:
+        country = findCountryWithMaxDeaths(min_date, max_date)
+        return {"success": True, "country": country}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
+
+
+@app.get("/min_deaths")
+async def get_country_with_min_deaths(min_date: str = Query(None), max_date: str = Query(None)):
+    """
+    Finds the country with the least deaths.
+    If min_date and max_date are provided, finds the country with the least deaths between the specified range of dates.
+    """
+    try:
+        country = findCountryWithMinDeaths(min_date, max_date)
+        return {"success": True, "country": country}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
+
+
+@app.get("/avg_deaths")
+async def get_average_deaths():
+    """Finds the average number of deaths between all countries."""
+    try:
+        avg_deaths = calculateAverageDeaths()
+        return {"success": True, "average_deaths": avg_deaths}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
 
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="127.0.0.1", port=5000, log_level="debug", reload=True) #host="127.0.0.1"
+    uvicorn.run("api:app", host="localhost", port=8080, log_level="debug", reload=True)
